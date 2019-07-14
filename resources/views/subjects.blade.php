bbe27f25dfa58a85be5a63aa23946e1e9a0a04e0
SamithaDilan <SamithaDilan@users.noreply.github.com>
2019-07-14 20:45:52 +0530

    mofified in v.1.0f3

--- resources/views/subjects.blade.php
+++ resources/views/subjects.blade.php
@@ -637,21 +637,7 @@
 					"aaSorting": [],
 					
 					
-					//"bProcessing": true,
-			        //"bServerSide": true,
-			        //"sAjaxSource": "http://127.0.0.1/table.php"	,
-			
-					//,
-					//"sScrollY": "200px",
-					//"bPaginate": false,
-			
-					//"sScrollX": "100%",
-					//"sScrollXInner": "120%",
-					//"bScrollCollapse": true,
-					//Note: if you are applying horizontal scrolling (sScrollX) on a ".table-bordered"
-					//you may want to wrap the table inside a "div.dataTables_borderWrap" element
-			
-					//"iDisplayLength": 50
+					
 			
 			
 					select: {
